ef47ae2fcada696a91665c0b0cd538f09321bcad
Szymon <smalek579@interia.pl>
2024-03-14 18:18:15 +0100

    congo

--- RS232/Program.cs
+++ RS232/Program.cs
@@ -8,10 +8,11 @@ namespace RS232
         [STAThread]
         static void Main()
         {
+            //
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
+            Application.Run(new Form1());
         }
     }
 }
